eaf2597832d8fc34a0aff30714978ae571f8b68c
Timotej Rozina <timotej.rozina1@gmail.com>
2022-11-03 12:40:12 +0100

    Animation preperation

--- player.py
+++ player.py
@@ -15,7 +15,9 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.frames = {"front": import_folder("../Assets/player/front/game/"),
-                       "run": import_folder("../Assets/player/side/right/game1/"), "jump": [], "fall": [],
+                       "run": import_folder("../Assets/player/side/right/game/"),
+                       "jump": [],
+                       "fall": import_folder("../Assets/player/front/game/"),
                        "holdWall": []}
         self.dir_i = "front"
         self.frame_index = 0
@@ -37,6 +39,17 @@ class Player(pygame.sprite.Sprite):
         self.jumpHeight = -17
 
     def animate(self):
+        if self.direction.x > 0:
+            self.dir_i = "run"
+            self.facing_right = True
+        if self.direction.x < 0:
+            self.dir_i = "run"
+            self.facing_right = False
+        if self.direction.x == 0:
+            self.dir_i = "front"
+        if self.direction.y > self.gravity:
+            self.dir_i = "fall"
+
         self.frame_index += 0.02 * self.animMult[self.dir_i]
         if self.frame_index >= len(self.frames[self.dir_i]):
             self.frame_index = 0
@@ -50,30 +63,22 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
-            self.dir_i = "run"
-            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
-            self.dir_i = "run"
-            self.facing_right = False
         else:
             self.direction.x = 0
-            self.dir_i = "front"
 
         if keys[pygame.K_UP] and self.on_wall and not self.wall_jumped and self.jumpTime > 15:
-            self.wall_jumped = True
             if self.facing_right:
                 self.direction.x = -1
-                self.direction.y += self.jumpHeight
-                self.jumps = 2
-                self.rect.x += self.direction.x * self.speed * 2.5
                 self.facing_right = False
             else:
                 self.direction.x = 1
-                self.direction.y += self.jumpHeight
-                self.jumps = 2
-                self.rect.x += self.direction.x * self.speed * 2.5
                 self.facing_right = True
+            self.direction.y += self.jumpHeight
+            self.rect.x += self.direction.x * self.speed * 2
+            self.jumps = 2
+            self.wall_jumped = True
             self.on_wall = False
             self.jumpTime = 0
 
