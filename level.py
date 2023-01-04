bfe61177bd59286aa9670f91c50fb7dcf12b28ea
Timotej Rozina <timotej.rozina1@gmail.com>
2023-01-04 17:39:51 +0100

    Tp

--- level.py
+++ level.py
@@ -91,14 +91,14 @@ class Level:
 
     def cam(self):  # ce je igralec znotraj 2 in 3 četrtine se kamera ne premika, drugače se
         player = self.player.sprite
-        if player.rect.centerx > (3 * self.settings.screen_w / 4) and player.direction.x > 0:
+        if player.rect.right > (3 * self.settings.screen_w / 4) and player.direction.x > 0:
             self.move = -player.speedInfo
             player.speed = 0
-        elif player.rect.centerx < (self.settings.screen_w / 4) and player.direction.x < 0:
+        elif player.rect.left < (self.settings.screen_w / 4) and player.direction.x < 0:
             self.move = player.speedInfo
             player.speed = 0
         else:
-            self.move = 0
+            self.move = int(self.move*0.999)
             player.speed = player.speedInfo
 
     def h_col_player(self):
