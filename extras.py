a72536ff4c4fbf7e680f0026bd82277565caa25b
Timotej Rozina <timotej.rozina1@gmail.com>
2023-02-05 17:33:46 +0100

    Game

--- extras.py
+++ extras.py
@@ -4,10 +4,10 @@ from settingMenu import Button
 from lvlSelect import Block
 
 class Text(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h, settings):
+    def __init__(self, x, y, settings, txt):
         super().__init__()
         self.settings = settings
-        self.image = settings.license
+        self.image = txt
         self.rect = self.image.get_rect(topleft=(x, y))
 class About:
     def __init__(self, surface, settings):
@@ -21,13 +21,16 @@ class About:
     def init_menu(self):
         self.groupButtons = pygame.sprite.Group()
         self.ui = pygame.sprite.Group()
+        self.text = pygame.sprite.Group()
         self.groupButtons.add(Button(self.settings.screen_w / 2 - 25, 650, "settings", "main_menu", self.settings))
         self.ui.add(Block(0, 0, self.settings.screen_w, self.settings.screen_h / 5, self.settings))
+        self.text.add(Text(90, 179, self.settings, self.settings.license))  # 255,255,0
 
     def draw(self):
         self.state = "about"
         self.settings.background.draw(self.display_surface)
         self.groupButtons.update(self)
+        self.text.draw(self.display_surface)
         self.ui.draw(self.display_surface)
         self.settings.logo.draw(self.display_surface)
         self.groupButtons.draw(self.display_surface)
@@ -50,7 +53,7 @@ class License:
         self.text = pygame.sprite.Group()
         self.groupButtons.add(Button(self.settings.screen_w / 2 - 25, 650, "main", "main_menu", self.settings))
         self.ui.add(Block(0, 0, self.settings.screen_w, self.settings.screen_h / 5, self.settings))
-        self.text.add(Text(90, 179, self.settings.screen_w, self.settings.screen_h / 5, self.settings)) #255,255,0
+        self.text.add(Text(90, 179, self.settings, self.settings.license)) #255,255,0
 
     def draw(self):
         self.state = "license"
